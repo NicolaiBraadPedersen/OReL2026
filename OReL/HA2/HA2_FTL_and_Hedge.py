cc68a6a5f4c5a96cdac92065bfe7d3592ba5e2ce
Zipzap007 <nicolai5047@gmail.com>
2026-02-15 16:38:09 +0100

    FTL and Hedge

--- OReL/HA2/HA2_FTL_and_Hedge.py
+++ OReL/HA2/HA2_FTL_and_Hedge.py
@@ -9,7 +9,6 @@ import pandas as pd
 #mpl.rcParams['axes.spines.bottom'] = False
 import matplotlib.pyplot as plt
 
-
 def run_algo(mu, T, func):
     loss = np.zeros(2)
     regret = np.zeros(T)
@@ -21,8 +20,35 @@ def run_algo(mu, T, func):
 
     for t in range(T):
         a_t = func(loss, K=2, T=T, t=t)
-        np.random.seed(t+42)
         x_t = np.random.binomial(1, mu)
+        if x_t == a_t:
+            loss[~a_t] += 1
+        else:
+            loss[a_t] += 1
+        #print(loss)
+
+        if a_t != opt:
+            regret[t] = delta
+
+    return np.cumsum(regret)
+
+def run_algo_newx(mu, T, func):
+    loss = np.zeros(2)
+    regret = np.zeros(T)
+
+    mu_0 = mu
+    mu_1 = 1-mu
+    delta = np.abs(mu_0-mu_1)
+    opt = np.argmin([mu_0,mu_1])
+
+    for t in range(T):
+        a_t = func(loss, K=2, T=T, t=t)
+
+        if t >= T/2 - 1 :
+            x_t = 0
+        else:
+            x_t = 1
+
         if x_t == a_t:
             loss[~a_t] += 1
         else:
@@ -50,13 +76,13 @@ def Hedge_T_8(loss, K, T, t):
     return int(a)
 
 def Hedge_t(loss, K, T, t):
-    eta_t = np.sqrt(np.log(K)/t+1)
+    eta_t = np.sqrt(np.log(K)/(t+1))
     p = np.exp(-eta_t * loss)/ sum(np.exp(-eta_t * loss))
     a = np.random.choice([0,1], p=p)
     return int(a)
 
 def Hedge_t_2(loss, K, T, t):
-    eta_t = 2*np.sqrt(np.log(K)/t+1)
+    eta_t = 2*np.sqrt(np.log(K)/(t+1))
     p = np.exp(-eta_t * loss)/ sum(np.exp(-eta_t * loss))
     a = np.random.choice([0,1], p=p)
     return int(a)
@@ -78,9 +104,60 @@ FTL_1 = np.array([run_algo(1/2-1/8,2000,FTL) for i in range(10)]).mean(axis=0)
 FTL_2 = np.array([run_algo(1/2-1/16,2000,FTL) for i in range(10)]).mean(axis=0)
 
 mu_0 = [1/2-1/4, 1/2-1/8, 1/2-1/16]
+colors = [
+    "#1f77b4",  # blue
+    "#ff7f0e",  # orange
+    "#2ca02c",  # green
+    "#d62728",  # red
+    "#9467bd",  # purple
+    "#8c564b",  # brown
+]
 
-Hedge_0 = np.array([run_algo(1/2-1/4,2000,Hedge_t) for i in range(1000)]).mean(axis=0)
+for mu in mu_0:
+    FTL_0, FTL_0_se = np.array([run_algo(mu,2000,FTL) for i in range(10)]).mean(axis=0) , np.array([run_algo(mu,2000,FTL) for i in range(10)]).std(axis=0)
+    Hedge_0, Hedge_0_se = np.array([run_algo(mu,2000,Hedge_t) for i in range(10)]).mean(axis=0), np.array([run_algo(mu,2000,Hedge_t) for i in range(10)]).std(axis=0)
+    Hedge_1, Hedge_1_se = np.array([run_algo(mu,2000,Hedge_t_2) for i in range(10)]).mean(axis=0), np.array([run_algo(mu,2000,Hedge_t_2) for i in range(10)]).std(axis=0)
+    Hedge_2, Hedge_2_se = np.array([run_algo(mu,2000,Hedge_T_2) for i in range(10)]).mean(axis=0), np.array([run_algo(mu,2000,Hedge_T_2) for i in range(10)]).std(axis=0)
+    Hedge_3, Hedge_3_se = np.array([run_algo(mu,2000,Hedge_T_8) for i in range(10)]).mean(axis=0), np.array([run_algo(mu,2000,Hedge_T_8) for i in range(10)]).std(axis=0)
+    plt.plot(FTL_0, label='FTL', color = colors[0])
+    plt.plot(FTL_0_se, label='FTL | se.', linestyle='dashed', color = colors[0])
+    plt.plot(Hedge_2, label='Hedge | eta reg.', color = colors[1])
+    plt.plot(Hedge_2_se, label='Hedge | eta reg. | + se.', linestyle='dashed', color = colors[1])
+    plt.plot(Hedge_3, label='Hedge | eta repar.', color = colors[2])
+    plt.plot(Hedge_3_se, label='Hedge | eta repar. | + se.', linestyle='dashed', color = colors[2])
+    plt.plot(Hedge_0, label='Hedge | eta 7.8', color = colors[3])
+    plt.plot(Hedge_0_se, label='Hedge | eta 7.8 | + se.', linestyle='dashed', color = colors[3])
+    plt.plot(Hedge_1, label='Hedge | eta 7.8 tight', color = colors[4])
+    plt.plot(Hedge_1_se, label='Hedge | eta 7.8 tight | + se.', linestyle='dashed', color = colors[4])
+    plt.legend()
+    plt.grid(True)
+    plt.xlabel('T')
+    plt.ylabel('Pseudo-regret')
+    plt.title(f'Hedge vs FTL | mu = {mu}')
+    #plt.show()
+    plt.savefig(f'C:\\Users\\nicol\\OneDrive - University of Copenhagen\\Desktop\\4 år\\OReL\\HA2\\Hedge vs FTL {mu}.png')
 
-plt.plot(Hedge_0)
-#plt.plot(FTL)
-plt.show()
+for mu in mu_0:
+    FTL_0, FTL_0_se = np.array([run_algo_newx(mu,2000,FTL) for i in range(10)]).mean(axis=0) , np.array([run_algo_newx(mu,2000,FTL) for i in range(10)]).std(axis=0)
+    Hedge_0, Hedge_0_se = np.array([run_algo_newx(mu,2000,Hedge_t) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_t) for i in range(10)]).std(axis=0)
+    Hedge_1, Hedge_1_se = np.array([run_algo_newx(mu,2000,Hedge_t_2) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_t_2) for i in range(10)]).std(axis=0)
+    Hedge_2, Hedge_2_se = np.array([run_algo_newx(mu,2000,Hedge_T_2) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_T_2) for i in range(10)]).std(axis=0)
+    Hedge_3, Hedge_3_se = np.array([run_algo_newx(mu,2000,Hedge_T_8) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_T_8) for i in range(10)]).std(axis=0)
+    plt.plot(FTL_0, label='FTL', color = colors[0])
+    plt.plot(FTL_0 + FTL_0_se, label='FTL | se.', linestyle='dashed', color = colors[0])
+    plt.plot(Hedge_2, label='Hedge | eta reg.', color = colors[1])
+    plt.plot(Hedge_2 + Hedge_2_se, label='Hedge | eta reg. | + se.', linestyle='dashed', color = colors[1])
+    plt.plot(Hedge_3, label='Hedge | eta repar.', color = colors[2])
+    plt.plot(Hedge_3 + Hedge_3_se, label='Hedge | eta repar. | + se.', linestyle='dashed', color = colors[2])
+    plt.plot(Hedge_0, label='Hedge | eta 7.8', color = colors[3])
+    plt.plot(Hedge_0 + Hedge_0_se, label='Hedge | eta 7.8 | + se.', linestyle='dashed', color = colors[3])
+    plt.plot(Hedge_1, label='Hedge | eta 7.8 tight', color = colors[4])
+    plt.plot(Hedge_1 + Hedge_1_se, label='Hedge | eta 7.8 tight | + se.', linestyle='dashed', color = colors[4])
+    plt.legend()
+    plt.grid(True)
+    plt.xlabel('T')
+    plt.ylabel('Pseudo-regret')
+    plt.title(f'Hedge vs FTL | mu = {mu} | New x')
+    #plt.show()
+    plt.savefig(
+        f'C:\\Users\\nicol\\OneDrive - University of Copenhagen\\Desktop\\4 år\\OReL\\HA2\\Hedge vs FTL {mu} new.png')
