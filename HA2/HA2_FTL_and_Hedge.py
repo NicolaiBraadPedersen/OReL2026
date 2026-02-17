7a8b4e49b0a83e7118d3c3c7969b04b010402770
Zipzap007 <nicolai5047@gmail.com>
2026-02-17 19:16:06 +0100

    updates

--- HA2/HA2_FTL_and_Hedge.py
+++ HA2/HA2_FTL_and_Hedge.py
@@ -32,13 +32,10 @@ def run_algo(mu, T, func):
 
     return np.cumsum(regret)
 
-def run_algo_newx(mu, T, func):
+def run_algo_newx(T, func):
     loss = np.zeros(2)
     regret = np.zeros(T)
     loss_realized = 0
-
-    mu_0 = mu
-    mu_1 = 1-mu
 
     for t in range(T):
         a_t = func(loss, K=2, T=T, t=t)
@@ -123,27 +120,27 @@ for mu in mu_0:
     plt.savefig(f'C:\\Users\\nicol\\OneDrive - University of Copenhagen\\Desktop\\4 år\\OReL\\HA2\\Hedge vs FTL {mu}.png')
     plt.show()
 
-for mu in mu_0:
-    FTL_0, FTL_0_se = np.array([run_algo_newx(mu,2000,FTL) for i in range(10)]).mean(axis=0) , np.array([run_algo_newx(mu,2000,FTL) for i in range(10)]).std(axis=0)
-    Hedge_0, Hedge_0_se = np.array([run_algo_newx(mu,2000,Hedge_t) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_t) for i in range(10)]).std(axis=0)
-    Hedge_1, Hedge_1_se = np.array([run_algo_newx(mu,2000,Hedge_t_2) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_t_2) for i in range(10)]).std(axis=0)
-    Hedge_2, Hedge_2_se = np.array([run_algo_newx(mu,2000,Hedge_T_2) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_T_2) for i in range(10)]).std(axis=0)
-    Hedge_3, Hedge_3_se = np.array([run_algo_newx(mu,2000,Hedge_T_8) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_T_8) for i in range(10)]).std(axis=0)
-    plt.plot(FTL_0, label='FTL', color = colors[0])
-    plt.plot(FTL_0 + FTL_0_se, label='FTL | se.', linestyle='dashed', color = colors[0])
-    plt.plot(Hedge_2, label='Hedge | eta reg.', color = colors[1])
-    plt.plot(Hedge_2 + Hedge_2_se, label='Hedge | eta reg. | + se.', linestyle='dashed', color = colors[1])
-    plt.plot(Hedge_3, label='Hedge | eta repar.', color = colors[2])
-    plt.plot(Hedge_3 + Hedge_3_se, label='Hedge | eta repar. | + se.', linestyle='dashed', color = colors[2])
-    plt.plot(Hedge_0, label='Hedge | eta 7.8', color = colors[3])
-    plt.plot(Hedge_0 + Hedge_0_se, label='Hedge | eta 7.8 | + se.', linestyle='dashed', color = colors[3])
-    plt.plot(Hedge_1, label='Hedge | eta 7.8 tight', color = colors[4])
-    plt.plot(Hedge_1 + Hedge_1_se, label='Hedge | eta 7.8 tight | + se.', linestyle='dashed', color = colors[4])
-    plt.legend()
-    plt.grid(True)
-    plt.xlabel('T')
-    plt.ylabel('Regret')
-    plt.title(f'Hedge vs FTL | mu = {mu} | New x')
-    plt.savefig(
-        f'C:\\Users\\nicol\\OneDrive - University of Copenhagen\\Desktop\\4 år\\OReL\\HA2\\Hedge vs FTL {mu} new.png')
-    plt.show()
+
+FTL_0, FTL_0_se = np.array([run_algo_newx(mu,2000,FTL) for i in range(10)]).mean(axis=0) , np.array([run_algo_newx(mu,2000,FTL) for i in range(10)]).std(axis=0)
+Hedge_0, Hedge_0_se = np.array([run_algo_newx(mu,2000,Hedge_t) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_t) for i in range(10)]).std(axis=0)
+Hedge_1, Hedge_1_se = np.array([run_algo_newx(mu,2000,Hedge_t_2) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_t_2) for i in range(10)]).std(axis=0)
+Hedge_2, Hedge_2_se = np.array([run_algo_newx(mu,2000,Hedge_T_2) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_T_2) for i in range(10)]).std(axis=0)
+Hedge_3, Hedge_3_se = np.array([run_algo_newx(mu,2000,Hedge_T_8) for i in range(10)]).mean(axis=0), np.array([run_algo_newx(mu,2000,Hedge_T_8) for i in range(10)]).std(axis=0)
+plt.plot(FTL_0, label='FTL', color = colors[0])
+plt.plot(FTL_0 + FTL_0_se, label='FTL | se.', linestyle='dashed', color = colors[0])
+plt.plot(Hedge_2, label='Hedge | eta reg.', color = colors[1])
+plt.plot(Hedge_2 + Hedge_2_se, label='Hedge | eta reg. | + se.', linestyle='dashed', color = colors[1])
+plt.plot(Hedge_3, label='Hedge | eta repar.', color = colors[2])
+plt.plot(Hedge_3 + Hedge_3_se, label='Hedge | eta repar. | + se.', linestyle='dashed', color = colors[2])
+plt.plot(Hedge_0, label='Hedge | eta 7.8', color = colors[3])
+plt.plot(Hedge_0 + Hedge_0_se, label='Hedge | eta 7.8 | + se.', linestyle='dashed', color = colors[3])
+plt.plot(Hedge_1, label='Hedge | eta 7.8 tight', color = colors[4])
+plt.plot(Hedge_1 + Hedge_1_se, label='Hedge | eta 7.8 tight | + se.', linestyle='dashed', color = colors[4])
+plt.legend()
+plt.grid(True)
+plt.xlabel('T')
+plt.ylabel('Regret')
+plt.title(f'Hedge vs FTL New x')
+plt.savefig(
+    f'C:\\Users\\nicol\\OneDrive - University of Copenhagen\\Desktop\\4 år\\OReL\\HA2\\Hedge vs FTL {mu} new.png')
+plt.show()
